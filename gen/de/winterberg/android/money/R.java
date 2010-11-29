93e5086d98932e2d1900f81416798a3488a41219
Benjamin Winterberg <winterbe@googlemail.com>
2010-11-29 19:27:24 +0100

    add menu

--- gen/de/winterberg/android/money/R.java
+++ gen/de/winterberg/android/money/R.java
@@ -14,14 +14,21 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int category_item=0x7f050000;
+        public static final int about=0x7f060002;
+        public static final int category_item=0x7f060000;
+        public static final int settings=0x7f060001;
     }
     public static final class layout {
         public static final int item=0x7f030000;
         public static final int main=0x7f030001;
     }
+    public static final class menu {
+        public static final int menu=0x7f050000;
+    }
     public static final class string {
+        public static final int about_label=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int empty_categories=0x7f040001;
+        public static final int settings_label=0x7f040002;
     }
 }
