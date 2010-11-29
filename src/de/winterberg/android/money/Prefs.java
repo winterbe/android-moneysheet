41340e04eaed82e359d68fb83d01d95d043e165a
Benjamin Winterberg <winterbe@googlemail.com>
2010-11-29 20:27:13 +0100

    add settings

---
+++ src/de/winterberg/android/money/Prefs.java
@@ -0,0 +1,18 @@
+package de.winterberg.android.money;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+/**
+ * Preferences activity.
+ *
+ * @author Benjamin Winterberg
+ */
+public class Prefs extends PreferenceActivity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+
+}
