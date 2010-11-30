1725c23924d2ceb09c483aab112bded92555172f
Benjamin Winterberg <winterbe@googlemail.com>
2010-11-30 07:16:47 +0100

    add settings

---
+++ src/de/winterberg/android/money/About.java
@@ -0,0 +1,18 @@
+package de.winterberg.android.money;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Activity for displaying an about dialog.
+ * 
+ * @author Benjamin Winterberg
+ */
+public class About extends Activity {
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about);
+    }
+
+}
