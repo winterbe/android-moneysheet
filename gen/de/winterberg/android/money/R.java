b0a595788349b8522dc6711e76c082c6ea5152cb
Benjamin Winterberg <winterbe@googlemail.com>
2010-12-06 08:10:16 +0100

    remove obsolete default icons

--- gen/de/winterberg/android/money/R.java
+++ gen/de/winterberg/android/money/R.java
@@ -16,7 +16,6 @@ public final class R {
         public static final int ic_menu_close_clear_cancel=0x7f020002;
         public static final int ic_menu_info_details=0x7f020003;
         public static final int ic_menu_preferences=0x7f020004;
-        public static final int icon=0x7f020005;
     }
     public static final class id {
         public static final int about=0x7f070017;
