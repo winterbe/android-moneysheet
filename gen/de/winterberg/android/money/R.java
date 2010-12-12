b669f17f80d20837b25af7b3780933b152582c99
Benjamin Winterberg <winterbe@googlemail.com>
2010-12-12 13:27:44 +0100

    disable settings

--- gen/de/winterberg/android/money/R.java
+++ gen/de/winterberg/android/money/R.java
@@ -36,12 +36,12 @@ public final class R {
         public static final int keypad=0x7f02000f;
     }
     public static final class id {
-        public static final int about=0x7f09001d;
+        public static final int about=0x7f09001c;
         public static final int add_category=0x7f09001b;
         public static final int amount_value=0x7f090000;
         public static final int category_item=0x7f090010;
         public static final int current_input_value=0x7f090001;
-        public static final int exit=0x7f09001e;
+        public static final int exit=0x7f09001d;
         public static final int history_amount=0x7f090013;
         public static final int history_diff=0x7f090012;
         public static final int history_time=0x7f090011;
@@ -59,7 +59,6 @@ public final class R {
         public static final int keypad_minus=0x7f09000f;
         public static final int keypad_plus=0x7f09000e;
         public static final int keypad_separator=0x7f09000c;
-        public static final int settings=0x7f09001c;
         public static final int stats_current_amount=0x7f090014;
         public static final int stats_from=0x7f090015;
         public static final int stats_sum_day=0x7f090017;
